5ff16a7134f17330f7e6e7ec9b968494d10158ce
Ypsillonn <35596935+Ypsillonn@users.noreply.github.com>
2018-05-17 15:15:51 +0200

    Update Jeu des batonnets.py

--- Jeu des batonnets.py
+++ Jeu des batonnets.py
@@ -12,42 +12,53 @@ j1 = 'Joueur 1'
 j2 = 'Joueur 2'
 
 
-def jouer():
+def Fjouer():
+    global jvo, jvj
     go.place_forget()
+    jvj = Button(menu, text='Contre un autre joueur', command=Fjvj)
+    jvo = Button(menu, text="Contre l'ordinateur", command=Fjvo)
     jvj.place(relx=0.5, rely=0.45, anchor=CENTER)
     jvo.place(relx=0.5, rely=0.55, anchor=CENTER)
 
-def jvj():
-    global Nom, Nom2, j1, j2
-    jvj.place_forget()
-    jvo.place_forget()
+def Fjvj():
+    global Nom, Nom2, j1, j2, jvo, jvj, appliquer
+    jvj.destroy()
+    jvo.destroy()
+    Nom = Entry(menu, bg="dodger blue")
+    Nom2 = Entry(menu, bg="firebrick1")
     Nom.insert(END, 'Joueur 1')
     Nom2.insert(END, 'Joueur 2')
     Nom.place(relx=0.5, rely=0.3, anchor=CENTER)
     Nom2.place(relx=0.5, rely=0.4, anchor=CENTER)
+    appliquer = Button(menu, text="Appliquer", command=Fname)
     appliquer.place(relx=0.5, rely=0.6, anchor=CENTER)
 
-def name():
-    global j1, j2
+def Fname():
+    global j1, j2, appliquer, Nom, Nom2, quiCommence, choix, bouton1, bouton2, bouton3
+    quiCommence = Label(menu, text="Qui commence ?", bg="white")
     quiCommence.place(relx=0.5, rely=0.1, anchor=CENTER)
+    j1 = Nom.get()
+    j2 = Nom2.get()
+    Nom.destroy()
+    Nom2.destroy()
+    appliquer.destroy()
+    choix = IntVar()
+    bouton1 = Radiobutton(menu, text=j1, variable=choix, value=1, command=Fcallback, bg="white")
+    bouton2 = Radiobutton(menu, text=j2, variable=choix, value=2, command=Fcallback, bg="white")
+    bouton3 = Radiobutton(menu, text="Aléatoire", variable=choix, value=3, command=Fcallback, bg="white")
     bouton1.place(relx=0.5, rely=0.15, anchor=CENTER)
     bouton2.place(relx=0.5, rely=0.25, anchor=CENTER)
     bouton3.place(relx=0.5, rely=0.35, anchor=CENTER)
-    j1 = Nom.get()
-    j2 = Nom2.get()
-    Nom.place_forget()
-    Nom2.place_forget()
-    appliquer.place_forget()
 
-def ordre():
-    global j1, j2, callbackVar
+def Fordre():
+    global j1, j2, callbackVar, bouton1, bouton2, bouton3, quiCommence
     callbackVar = 0
     menu['bg']='lightgray'
     ordre.place_forget()
-    bouton1.place_forget()
-    bouton2.place_forget()
-    bouton3.place_forget()
-    quiCommence.place_forget()
+    bouton1.destroy()
+    bouton2.destroy()
+    bouton3.destroy()
+    quiCommence.destroy()
     if choix.get() == 1:
        menu.geometry("1000x500")
        print(j1)
@@ -57,87 +68,135 @@ def ordre():
     else :
         menu.geometry("1000x500")
 
-def jvo():
-    jvo.place_forget()
-    jvj.place_forget()
+def Fjvo():
+    global jvo, jvj, soft, normal, hard, impossible
+    jvo.destroy()
+    jvj.destroy()
+    soft = Button(menu, text="Facile", command=Fsoft)
+    normal = Button(menu, text="Normal", command=Fnormal)
+    hard = Button(menu, text="Difficile", command=Fhard)
+    impossible = Button(menu, text="Impossible", command=Fimpossible)
     soft.place(relx=0.5, rely=0.35, anchor=CENTER)
     normal.place(relx=0.5, rely=0.45, anchor=CENTER)
     hard.place(relx=0.5, rely=0.55, anchor=CENTER)
     impossible.place(relx=0.5, rely=0.65, anchor=CENTER)
 
-def soft():
-    global difficulty
-    soft.place_forget()
-    normal.place_forget()
-    hard.place_forget()
-    impossible.place_forget()
+def Fsoft():
+    global soft, normal, hard, impossible, difficulty
+    soft.destroy()
+    normal.destroy()
+    hard.destroy()
+    impossible.destroy()
     menu.geometry("1000x500")
     menu['bg']='lightgray'
     difficulty = 1
 
-def normal():
-    global difficulty
-    soft.place_forget()
-    normal.place_forget()
-    hard.place_forget()
-    impossible.place_forget()
+def Fnormal():
+    global soft, normal, hard, impossible, difficulty
+    soft.destroy()
+    normal.destroy()
+    hard.destroy()
+    impossible.destroy()
     menu.geometry("1000x500")
     menu['bg']='lightgray'
     difficulty = 2
 
-def hard():
-    global difficulty
-    soft.place_forget()
-    normal.place_forget()
-    hard.place_forget()
-    impossible.place_forget()
+def Fhard():
+    global soft, normal, hard, impossible, difficulty
+    soft.destroy()
+    normal.destroy()
+    hard.destroy()
+    impossible.destroy()
     menu.geometry("1000x500")
     menu['bg']='lightgray'
     difficulty = 3
 
-def impossible():
-    global difficulty
-    soft.place_forget()
-    normal.place_forget()
-    hard.place_forget()
-    impossible.place_forget()
+def Fimpossible():
+    global soft, normal, hard, impossible, difficulty
+    soft.destroy()
+    normal.destroy()
+    hard.destroy()
+    impossible.destroy()
     menu.geometry("1000x500")
     menu['bg']='lightgray'
     difficulty = 4
 
-def retour():
-    global callbackVar, difficulty
+def Fretour():
+    global callbackVar, jvo, jvj, soft, normal, hard, impossible, difficulty, bouton1, bouton2, bouton3, quiCommence, appliquer, Nom, Nom2, ordre
     callbackVar = 0
     difficulty = 0
-    soft.place_forget()
-    normal.place_forget()
-    hard.place_forget()
-    impossible.place_forget()
-    jvo.place_forget()
-    jvj.place_forget()
-    ordre.place_forget()
-    bouton1.place_forget()
-    bouton2.place_forget()
-    bouton3.place_forget()
-    Nom.place_forget()
-    Nom2.place_forget()
-    appliquer.place_forget()
-    quiCommence.place_forget()
+    try :
+        soft.destroy()
+    except NameError:
+        pass
+    try :
+        normal.destroy()
+    except NameError:
+        pass
+    try :
+        hard.destroy()
+    except NameError:
+        pass
+    try :
+        impossible.destroy()
+    except NameError:
+        pass
+    try :
+        jvo.destroy()
+    except NameError:
+        pass
+    try :
+        jvj.destroy()
+    except NameError:
+        pass
+    try :
+        ordre.destroy()
+    except NameError:
+        pass
+    try :
+        bouton1.destroy()
+    except NameError:
+        pass
+    try :
+        bouton2.destroy()
+    except NameError:
+        bouton3.destroy()
+    except NameError:
+        pass
+    try :
+        Nom.destroy()
+    except NameError:
+        pass
+    try :
+        Nom2.destroy()
+    except NameError:
+        pass
+    try :
+        appliquer.destroy()
+    except NameError:
+        pass
+    try :
+        quiCommence.destroy()
+    except NameError:
+        pass
     go.place(relx=0.5, rely=0.5, anchor=CENTER)
     menu.geometry("500x500")
     menu['bg']='white'
 
-def callback():
-    global callbackVar
+def Fcallback():
+    global callbackVar, ordre
     if callbackVar == 0:
-       ordre.place(relx=0.5, rely=0.6, anchor=CENTER)
-       callbackVar = 1
+        ordre = Button(menu, text="Lancer", command=Fordre)
+        ordre.place(relx=0.5, rely=0.6, anchor=CENTER)
+        callbackVar = 1
 
-def aide():
+def Faide():
     if askyesno('Aide : Comment gagner ?', "Vous n'arrivez pas à gagner au jeu des bâtonnets ? Voulez-vous connaître l'astuce ?"):
         if askyesno('Aide : Comment gagner ?', "Le principe est très simple, pour gagner vous devez vous assurer de prendre l'avant dernier bâtonnet. Mais pour cela vous devez contrôler la partie. Voulez-vous savoir comment faire ?"):
             if askyesno('Aide : Comment gagner ?', "Comme l'on peut tirer 1 à 3 bâtonnets, cela signifie que vous pouvez toujours compléter le tirage de l'adversaire pour arriver à  4. Ainsi le but est d'aller de 4 en 4 jusqu'à l'avant dernier bâtonnet. Avez-vous besoin d'un exemple ?"):
                  showwarning('Aide : Comment gagner ?', "Prenons l'exemple d'une partie courte à 15 bâtonnets. En suivant le raisonemment précédent, vous devez pour gagner, obtenir le 14ème bâtonnet. Or pour l'atteindre vous devez aller de 4 en 4 et donc tenter de vous emparer des bâtonnets 2, 6 ou 10. Une fois ceci fait, il suffit de compléter le tirage de l'adversaire comme vu précédemment. Vous êtes maintenant au point, bonne chance !")
+
+
 
 menu = Tk()
 menu.title('Le jeu des bâtonnets')
@@ -146,32 +205,19 @@ menu.resizable(0, 0)
 menu['bg']='white'
 
 menubar = Menu(menu)
-
 menu1 = Menu(menubar, tearoff=100)
-menu1.add_command(label="Retour menu", command=retour)
-menu1.add_command(label="Aide", command=aide)
+menu1.add_command(label="Retour menu", command=Fretour)
+menu1.add_command(label="Aide", command=Faide)
 menu1.add_separator()
 menu1.add_command(label="Quitter", command=menu.destroy)
 menubar.add_cascade(label="Fichier", menu=menu1)
 
 
-go = Button(menu, text='Lancer la partie', command=jouer)
-go.place(relx=0.5, rely=0.5, anchor=CENTER)
-jvj = Button(menu, text='Contre un autre joueur', command=jvj)
-jvo = Button(menu, text="Contre l'ordinateur", command=jvo)
-soft = Button(menu, text="Facile", command=soft)
-normal = Button(menu, text="Normal", command=normal)
-hard = Button(menu, text="Difficile", command=hard)
-impossible = Button(menu, text="Impossible", command=impossible)
-appliquer = Button(menu, text="Appliquer", command=name)
-choix = IntVar()
-bouton1 = Radiobutton(menu, text="Joueur 1", variable=choix, value=1, command=callback, bg="white")
-bouton2 = Radiobutton(menu, text="Joueur 2", variable=choix, value=2, command=callback, bg="white")
-bouton3 = Radiobutton(menu, text="Aléatoire", variable=choix, value=3, command=callback, bg="white")
-Nom = Entry(menu, bg="dodger blue")
-Nom2 = Entry(menu, bg="firebrick1")
 
-ordre = Button(menu, text="Lancer", command=ordre)
-quiCommence = Label(menu, text="Qui commence ?", bg="white")
+go = Button(menu, text='Lancer la partie', command=Fjouer)
+go.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+
+
 menu.config(menu=menubar)
 menu.mainloop()
